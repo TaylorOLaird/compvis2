7a81169de75ba56e56d00cd90186e4e874142252
Taylor Laird <taylorolaird@knights.ucf.edu>
2023-09-12 12:57:17 -0400

    more initial set up

---
+++ cifar10vit16.py
@@ -0,0 +1,72 @@
+import torch
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import random
+
+
+# helper functions to get the data in a form I can use for inference
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def get_data():
+    label_names = unpickle('cifar-10-batches-py/batches.meta')
+    labels = []
+    for i in range(len(label_names[b'label_names'])):
+        # strip the b' and ' from the label names
+        labels.append(label_names[b'label_names'][i].decode("utf-8"))
+
+
+    # load the image data
+    data = unpickle('cifar-10-batches-py/data_batch_1')
+    images_data = data[b'data']
+    targets = data[b'labels']
+
+    new_images_data = []
+
+    for img in images_data:
+        img = img.reshape(3, 32, 32)
+        img = img.transpose(1, 2, 0)
+        img = Image.fromarray(img, 'RGB')
+        new_images_data.append(img)
+
+    return new_images_data, targets, labels
+
+
+# step 1: grab a pretrained model
+model_name = "nateraw/vit-base-patch16-224-cifar10"
+model = ViTForImageClassification.from_pretrained(model_name)
+
+# step 2: get the cifar 10 data from our helper function
+images_data, targets, label_array = get_data()
+# this is for the video demo, just grab a single random image
+test = random.randint(0, len(images_data))
+image = images_data[test]
+
+# step 3: Use the ViT feature extractor to preprocess the image
+feature_extractor = ViTFeatureExtractor.from_pretrained(model_name)
+inputs = feature_extractor(images=image, return_tensors="pt")
+
+# Step 4: Run Inference
+outputs = model(**inputs)
+logits = outputs.logits  # Get the logits (raw scores) from the model
+probabilities = torch.nn.functional.softmax(logits, dim=-1)[0]
+
+# print the probabilities
+for label in range(len(label_array)):
+    print(f"{label_array[label]}: {probabilities[label].item() * 100:.2f}%")
+
+# Find the predicted class index with the highest probability
+predicted_class = torch.argmax(probabilities).item()
+predicted_label = label_array[predicted_class]
+
+print(f"Predicted label: {predicted_label}")
+print(f"Ground truth label: {label_array[targets[test]]}")
+
+print(f"are they the same? {predicted_label == label_array[targets[test]]}")
+
+# show the image
+image.show()
